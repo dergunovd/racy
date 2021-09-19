e56f6743d61c89f9bf98792c42fc0a09ba5edcaa
Dmitry Dergunov <dmitry@dergunov.net>
2021-09-19 19:55:59 +0300

    add ad block

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+// import {AdMobBanner} from 'react-native-admob';
 
 import {Map} from './components/Map';
 import {Info} from './components/Info';
@@ -8,6 +9,15 @@ import {StoreContextProvider} from './store';
 const App = () => (
   <SafeAreaView>
     <StoreContextProvider>
+      {/*<AdMobBanner*/}
+      {/*  adSize="banner"*/}
+      {/*  adUnitID="ca-app-pub-2714230443684738/6752267802"*/}
+      {/*  testDevices={[AdMobBanner.simulatorId]}*/}
+      {/*  width="100%"*/}
+      {/*  height={100}*/}
+      {/*  onAdLoaded={console.log}*/}
+      {/*  onAdFailedToLoad={(error: Error) => console.error(error)}*/}
+      {/*/>*/}
       <Map />
       <Info />
     </StoreContextProvider>
